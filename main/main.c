848d7b84ecbcd410b2b2ae28e65a7b8eb295b5aa
Mohamed Benomar <mohamed_3151@hotmail.com>
2023-01-06 15:06:16 -0800

    First Commit

---
+++ main/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include <wifi_config.h>
+
+
+const int led_gpio = 0;
+bool led_on = false;
+
+void led_write(bool on) {
+    gpio_write(led_gpio, on ? 0 : 1);
+}
+
+void led_init() {
+    gpio_enable(led_gpio, GPIO_OUTPUT);
+    led_write(led_on);
+}
+
+void led_identify_task(void *_args) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            led_write(true);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            led_write(false);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    led_write(led_on);
+
+    vTaskDelete(NULL);
+}
+
+void led_identify(homekit_value_t _value) {
+    printf("LED identify\n");
+    xTaskCreate(led_identify_task, "LED identify", 128, NULL, 2, NULL);
+}
+
+homekit_value_t led_on_get() {
+    return HOMEKIT_BOOL(led_on);
+}
+
+void led_on_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    led_on = value.bool_value;
+    led_write(led_on);
+}
+
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Neon"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Mohamed Benomar"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "9873216549"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Neon"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Neon"),
+            HOMEKIT_CHARACTERISTIC(
+                ON, false,
+                .getter=led_on_get,
+                .setter=led_on_set
+            ),
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "987-11-999",
+    .setupId="9SW7"
+};
+
+void on_wifi_ready() {
+    homekit_server_init(&config);
+}
+
+void user_init(void) {
+    uart_set_baud(0, 115200);
+
+    wifi_config_init("Neon", NULL, on_wifi_ready);
+    led_init();
+}
